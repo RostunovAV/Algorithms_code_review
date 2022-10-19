fd72b6453cac411321f46bd0ec494cb495f838dd
Artem <rostunov.av@phystech.edu>
2022-10-20 00:01:48 +0300

    TaskB code

--- Contest1TaskB/main.cpp
+++ Contest1TaskB/main.cpp
@@ -1,21 +1,19 @@
 #include <iostream>
 
-using namespace std;
 const int kKsize = 1e5 + 3;
 int arr[kKsize];
 
 int main() {
   int num, answer = 0;
-  cin >> num;
+  std::cin >> num;
 
   for (int i = 0; i < num; i++) {
-    cin >> arr[i];
+    std::cin >> arr[i];
     if (i != 0 && arr[i - 1] > arr[i]) {
       answer = i;
     }
   }
 
-  cout << answer;
-
+  std::cout << answer;
   return 0;
 }
