12ad9eb501f12d9579a45f072edfeae6c624bc7b
Artem <rostunov.av@phystech.edu>
2022-10-20 00:43:44 +0300

    TaskH code

--- Contest1TaskH/main.cpp
+++ Contest1TaskH/main.cpp
@@ -2,17 +2,16 @@
 #include <iomanip>
 #include <iostream>
 
-using namespace std;
 const int kSize = 3e5 + 3, kINF = 1e9 + 7;
 double pref[kSize];
 
 int main() {
   int sz;
-  cin >> sz;
+  std::cin >> sz;
 
   for (int i = 0; i < sz; i++) {
     double num;
-    cin >> num;
+    std::cin >> num;
     pref[i] += log(num);
     if (i > 0) {
       pref[i] += pref[i - 1];
@@ -20,19 +19,19 @@ int main() {
   }
 
   int questions;
-  cin >> questions;
-  cout << fixed << setprecision(40);
+  std::cin >> questions;
+  std::cout << std::fixed << std::setprecision(40);
 
   for (int i = 0; i < questions; i++) {
     int left, right;
-    cin >> left >> right;
+    std::cin >> left >> right;
     double sum_ln = pref[right];
     if (left > 0) {
       sum_ln -= pref[left - 1];
     }
     int len = right - left + 1;
     double res = exp(sum_ln / len);
-    cout << res << '\n';
+    std::cout << res << '\n';
   }
 
   return 0;
